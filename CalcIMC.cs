1bd3e10a87ce865fc3d8a36f4d52876cc148a121
Wanderson Duarte Alves <wandersondrtlvs.new@gmail.com>
2023-02-26 00:25:28 -0300

    first commit

---
+++ CalcIMC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalcIMC
+    {
+        public void calcimc(decimal p, decimal h) {
+            decimal value = Convert.ToDecimal(p / (h * h));
+
+            if (value < Convert.ToDecimal(18.5)) { Console.WriteLine("Você encontra-se na categoria: MAGREZA"); }
+            else if (value > Convert.ToDecimal(18.6) && value < Convert.ToDecimal(24.9)) { Console.WriteLine("Você encontra-se na categoria: PESO NORMAL"); }
+            else if (value > Convert.ToDecimal(25) && value < Convert.ToDecimal(29.9)) { Console.WriteLine("Você encontra-se na categoria: SOBREPESO"); }
+            else if (value > Convert.ToDecimal(30) && value < Convert.ToDecimal(34.9)) { Console.WriteLine("Você encontra-se na categoria: OBESIDADE DE GRAU I"); }
+            else if (value > Convert.ToDecimal(35) && value < Convert.ToDecimal(39.9)) { Console.WriteLine("Você encontra-se na categoria: OBESIDADE DE GRAU II"); }
+            else if (value > Convert.ToDecimal(40)) { Console.WriteLine("Você encontra-se na categoria: OBESIDADE DE GRAU III"); }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using Calculator;
+
+CalcIMC calc = new CalcIMC();
+bool value = false;
+do
+{
+    Console.WriteLine("############ CÁLCULO DO IMC ############");
+
+    Console.WriteLine("Digite sua altura: ");
+    decimal h = Convert.ToDecimal(Console.ReadLine());
+
+    Console.WriteLine("Digite seu peso: ");
+    decimal p = Convert.ToDecimal(Console.ReadLine());
+
+    calc.calcimc(p, h);
+
+    Console.WriteLine("Enter yes or no: ");
+    var inputString = Console.ReadLine();
+    if (String.IsNullOrEmpty(inputString))
+    {
+        continue;
+    }
+    if (string.Equals(inputString, "yes"))
+    {
+        value = true;
+    }
+    Console.Clear();
+} while (!value);
